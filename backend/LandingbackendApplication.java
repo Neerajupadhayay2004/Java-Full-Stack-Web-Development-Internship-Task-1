0c781f495c37921b9810dbf1c0f138b61b48c904
Neerajupadhayay2004 <nupadhayay219@gmail.com>
2026-03-02 20:59:57 +0530

    Full Stack Internship Task 1 - Frontend + Spring Boot Backend

---
+++ backend/LandingbackendApplication.java
@@ -0,0 +1,12 @@
+package com.neeraj.landingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandingbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandingbackendApplication.class, args);
+    }
+}
